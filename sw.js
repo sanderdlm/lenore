7a7d6effb89ced644d841e21a59f8ef177aa2723
Sander De la Marche <sander.de.la.marche@4411.be>
2026-01-20 19:22:07 +0100

    add pwa reload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_VERSION = 'v1.0.0';
+const CACHE_NAME = `bankan-${CACHE_VERSION}`;
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME)
+          .then((cache) => cache.put(event.request, responseToCache));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
